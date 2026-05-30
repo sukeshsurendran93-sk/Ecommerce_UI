009fda0f239cdbcb26c85ead85079922fce30ec3
Sukesh4230 <sukesh1993@gmail.com>
2026-05-31 00:31:45 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://ecommerce-backend-gj1w.onrender.com/api",
     timeout: 10000,
 });
 
